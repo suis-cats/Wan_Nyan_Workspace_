1f2486f0a0552e1b744e5ad55e6798b8b3823adf
suis-cats <takkunkimidori@gmail.com>
2023-08-24 14:11:25 +0900

    create reserve page

---
+++ resources/views/apply/reserve.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    @vite(['resources/css/indexpage.css'])
+    <x-slot name="header">
+        
+    </x-slot>
+    <style>
+      
+    </style>
+        <div class="d-flex align-items-center justify-content-center" style="min-height: 90vh;">
+            <div class="text-center">
+                <p>予約ページだよ</p>
+            </div>
+
+
+            <!-- 他のコンテンツを追加する場合はここに追加できます -->
+        </div>
+</x-app-layout>
